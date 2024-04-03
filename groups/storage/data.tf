62cbd9714bd1d2557685bf817b9216e1b5848a3e
Marc Ransome <mransome@companieshouse.gov.uk>
2024-04-03 09:57:58 +0100

    Enable server access logging and add second IAM user

--- groups/storage/data.tf
+++ groups/storage/data.tf
@@ -1,4 +1,6 @@
-data "aws_iam_policy_document" "data" {
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "accessor" {
   statement {
     sid = "AllowDataUserToListBucket"
 
@@ -29,7 +31,7 @@ data "aws_iam_policy_document" "data" {
   }
 }
 
-data "aws_iam_policy_document" "bucket" {
+data "aws_iam_policy_document" "data" {
   statement {
     sid = "DenyPutObjectWithKmsEncryptionHeader"
 
@@ -52,6 +54,116 @@ data "aws_iam_policy_document" "bucket" {
       test     = "StringEquals"
       variable = "s3:x-amz-server-side-encryption"
       values   = ["aws:kms"]
+    }
+  }
+
+  statement {
+    sid = "DenyNonSSLRequests"
+
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      aws_s3_bucket.data.arn,
+      "${aws_s3_bucket.data.arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "logs" {
+  statement {
+    sid = "AllowPutObjectS3ServerAccessLogs"
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["logging.s3.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.logs.arn}/logs/*"
+    ]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.data.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.aws_account_id]
+    }
+  }
+
+  statement {
+    sid = "RestrictToS3ServerAccessLogs"
+
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.logs.arn}/logs/*"
+    ]
+
+    condition {
+      test     = "ForAllValues:StringNotEquals"
+      variable = "aws:PrincipalServiceNamesList"
+      values   = ["logging.s3.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid = "DenyNonSSLRequests"
+
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      aws_s3_bucket.logs.arn,
+      "${aws_s3_bucket.logs.arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
     }
   }
 }
--- groups/storage/s3.tf
+++ groups/storage/s3.tf
@@ -3,6 +3,13 @@ resource "aws_s3_bucket" "data" {
   tags   = local.common_tags
 }
 
+resource "aws_s3_bucket_logging" "data" {
+  bucket = aws_s3_bucket.data.id
+
+  target_bucket = aws_s3_bucket.logs.id
+  target_prefix = "logs/"
+}
+
 resource "aws_s3_bucket_versioning" "data" {
   bucket = aws_s3_bucket.data.id
   versioning_configuration {
@@ -23,7 +30,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
 
 resource "aws_s3_bucket_policy" "data" {
   bucket = aws_s3_bucket.data.id
-  policy = data.aws_iam_policy_document.bucket.json
+  policy = data.aws_iam_policy_document.data.json
 }
 
 resource "aws_s3_bucket_public_access_block" "data" {
@@ -37,6 +44,44 @@ resource "aws_s3_bucket_public_access_block" "data" {
 
 resource "aws_s3_bucket_ownership_controls" "data" {
   bucket = aws_s3_bucket.data.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "${var.environment}.${var.service}-access-logs.${var.aws_account}.ch.gov.uk"
+  tags   = local.common_tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  policy = data.aws_iam_policy_document.logs.json
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "logs" {
+  bucket = aws_s3_bucket.logs.id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
