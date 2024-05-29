ebf2f65cb5e252e0960888fd95fb7ee3d6e1f2cf
Jimmy <jtaylor@companieshouse.gov.uk>
2024-05-29 14:10:55 +0100

    Add a 3rd accessor user

--- groups/storage/iam.tf
+++ groups/storage/iam.tf
@@ -8,12 +8,21 @@ resource "aws_iam_user" "data_2" {
   tags = local.common_tags
 }
 
+resource "aws_iam_user" "data_3" {
+  name = "${var.environment}-${var.service}-accessor-3"
+  tags = local.common_tags
+}
+
 resource "aws_iam_access_key" "data" {
   user = aws_iam_user.data.name
 }
 
 resource "aws_iam_access_key" "data_2" {
   user = aws_iam_user.data_2.name
+}
+
+resource "aws_iam_access_key" "data_3" {
+  user = aws_iam_user.data_3.name
 }
 
 resource "aws_iam_policy" "accessor" {
@@ -30,5 +39,10 @@ resource "aws_iam_user_policy_attachment" "data" {
 
 resource "aws_iam_user_policy_attachment" "data_2" {
   user       = aws_iam_user.data_2.name
+  policy_arn = aws_iam_policy.accessor.arn
+}
+
+resource "aws_iam_user_policy_attachment" "data_3" {
+  user       = aws_iam_user.data_3.name
   policy_arn = aws_iam_policy.accessor.arn
 }
